91fc661cb4f80f597759c823a30f38fa450f381a
Tommy Barnes <thbarnes@microsoft.com>
2017-10-30 11:45:49 -0400

    Changed MatSlideToggle component import to MatSlideToggleModule declaration because 'ng build --prod' was having trouble resolving with the former.

--- src/app/material-imports.ts
+++ src/app/material-imports.ts
@@ -1,5 +1,5 @@
 import { MatCardModule, MatListModule, MatRippleModule, MatIconModule, MatSnackBarModule, MatDialogModule,
-         MatTabsModule, MatToolbarModule, MatButtonModule, MatInputModule, MatSlideToggle } from '@angular/material';
+         MatTabsModule, MatToolbarModule, MatButtonModule, MatInputModule, MatSlideToggleModule } from '@angular/material';
 import { FocusMonitor } from '@angular/cdk/a11y';
 
 export const MaterialModules = [
@@ -12,12 +12,11 @@ export const MaterialModules = [
     MatTabsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatSlideToggleModule
 ];
 
-export const MaterialComponents = [
-    MatSlideToggle
-];
+export const MaterialComponents = [];
 
 export const MaterialServices = [
     FocusMonitor
